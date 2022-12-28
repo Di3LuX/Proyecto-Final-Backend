0150cce4e3810dd4a64e840fc8047bd709897886
Diego.W <Di3LuX_11@hotmail.es>
2022-12-28 16:46:13 +0100

    Created estate Model/Migration/Views/Seeders and endpoint to get all estates

--- router.js
+++ router.js
@@ -4,8 +4,10 @@ const auth = require('./middlewares/auth');
 
 const userRouter = require("./views/userRouter");
 const vehiclesRouter = require("./views/vehiclesRouter");
+const estatesRouter = require("./views/estatesRouter");
 
 router.use("/vehicles", vehiclesRouter);
+router.use("/estates", estatesRouter);
 router.use("/users", userRouter);
 
 module.exports = router;
